a26a3d231700dac784c1a3e8a3c1068b42c0b275
Ivan <ashikhin1998@gmail.com>
2023-04-20 01:54:37 +0600

    Сериализатор
    Исправил сериализатор

--- api/serializes.py
+++ api/serializes.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-
 from posts.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('created_at', 'comments_count', 'likes_count', 'description', 'image', 'author')
+        fields = ('id', 'created_at', 'comments_count', 'likes_count', 'description', 'image', 'author')
+        read_only_fields = ('id', 'created_at', 'likes_count', 'comments_count')
